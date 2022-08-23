27b01661bee9a41acef1cdc6dc6d7e2881b2ba53
yanziyue0 <111879645+yanziyue0@users.noreply.github.com>
2022-08-23 21:29:45 +0800

    Create Bootstrap.java

---
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -0,0 +1,39 @@
+package cn.ofpp;
+
+import cn.hutool.core.util.StrUtil;
+import cn.ofpp.core.Wx;
+
+/**
+ * 引导配置
+ *
+ * @author DokiYolo
+ * Date 2022-08-23
+ */
+public class Bootstrap {
+
+    /**
+     * 公众号AppID
+     */
+    public static final String APP_ID = "wx794935efdefe279a";
+
+    /**
+     * 公众号秘钥
+     */
+    public static final String SECRET = "a5e611a99012cda8307931ebee8f5ba4";
+
+    /**
+     * 全局模板ID  也可针对单个Friend指定模板
+     */
+    public static final String TEMPLATE_ID = "xIrljElc3sS4YI12m9GOIbqRQwu2K7EG8bZ3jeZM9Ws";
+
+    /**
+     * 初始化
+     */
+    public static void init() {
+        if (StrUtil.hasEmpty(APP_ID, SECRET, TEMPLATE_ID)) {
+            throw new IllegalArgumentException("请检查配置");
+        }
+        Wx.init();
+    }
+
+}
